06c7559c312996cd3225a63dfde270610ebf4c97
Shuvam <shuvamsantra10@gmail.com>
2024-02-04 09:38:33 +0530

    Changed navbar

--- src/utils/baseURL.ts
+++ src/utils/baseURL.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const devURL = "http://localhost:3000";
 
-// export const prodURL = "https://cypherscan-backend.onrender.com";
 export const prodURL = ""; // Render Server URL
 
 // Axios Instance - Specify the server to be used.
--- src/utils/prismadb.ts
+++ src/utils/prismadb.ts
@@ -3,11 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as any;
 
 export const prismadb: PrismaClient =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // log: ["query"],
-    // errorFormat: "pretty",
-  });
+  globalForPrisma.prisma || new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prismadb;
